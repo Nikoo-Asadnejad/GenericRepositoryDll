5453587c33c3c7d73336dfe6fe6d468f25760988
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-06-30 17:21:20 +0430

    Feat : add synchronous methods to the repository

--- GenericRepositoryDll/GenericRepositoryDll/Repository/GenericRepository/Repository.cs
+++ GenericRepositoryDll/GenericRepositoryDll/Repository/GenericRepository/Repository.cs
@@ -20,14 +20,29 @@ namespace GenericReositoryDll.Repository.GenericRepository
       await _context.SaveChangesAsync();
     }
 
+    public void Add(T model)
+    {
+       _model.Add(model);
+       Save();
+    }
+
     public async Task AddRangeAsync(IEnumerable<T> models)
     {
       await _model.AddRangeAsync(models);
       await _context.SaveChangesAsync();
     }
 
+    public void AddRange(IEnumerable<T> models)
+    {
+       _model.AddRange(models);
+       Save();
+    }
+
     public async Task<bool> AnyAsync(Expression<Func<T, bool>> query)
     => await _model.AnyAsync(query);
+
+    public bool Any(Expression<Func<T, bool>> query)
+    =>  _model.Any(query);
 
     public async Task DeleteAsync(long id)
     {
@@ -36,15 +51,31 @@ namespace GenericReositoryDll.Repository.GenericRepository
       await _context.SaveChangesAsync();
     }
 
+    public void Delete(long id)
+    {
+      T model = _context.Find<T>(id);
+      if (model != null) _model.Remove(model);
+      Save();
+    }
+
     public async Task DeleteRangeAsync(IEnumerable<T> models)
     {
       _model.RemoveRange(models);
       await _context.SaveChangesAsync();
     }
 
+    public void DeleteRange(IEnumerable<T> models)
+    {
+      _model.RemoveRange(models);
+      Save();
+    }
+
     public async Task<long> GetCountAsync(Expression<Func<T, bool>> query = null)
     => await _model.CountAsync(query);
-    
+
+    public long GetCount(Expression<Func<T, bool>> query = null)
+    =>  _model.Count(query);
+
     /// <summary>
     /// 
     /// </summary>
@@ -81,6 +112,32 @@ namespace GenericReositoryDll.Repository.GenericRepository
     }
 
 
+    public List<object> GetList(Expression<Func<T, bool>> query = null,
+      Func<T, object> selector = null,
+      Func<T, object> orderBy = null,
+      OrderType? orderType = null,
+      List<string> includes = null,
+      int? skip = null,
+      int? take = null,
+      bool? distinct = null)
+    {
+      dynamic result;
+      var models = _model.AsQueryable().AsNoTrackingWithIdentityResolution();
+      if (query != null) models = models.Where(query);
+      if (includes != null && includes.Count() > 0) includes.ForEach(includeProperty => models.Include(includeProperty));
+      if (skip != null) models = models.Skip((int)skip);
+      if (take != null) models = models.Take((int)take);
+      if (orderBy != null && orderType == OrderType.Asc) models = models.OrderBy(orderBy).AsQueryable();
+      if (orderBy != null && orderType == OrderType.Desc) models = models.OrderByDescending(orderBy).AsQueryable();
+      if (orderBy != null && orderType == null) models = models.OrderBy(orderBy).AsQueryable();
+      if (distinct != null) models.Distinct();
+      if (selector != null) result = models.Select(selector);
+      result = models;
+
+      return result.Tolist();
+
+    }
+
     public async Task<List<T>> GetListAsync<T>(Expression<Func<T, bool>> query = null,
      Func<T, object> orderBy = null,
      OrderType? orderType = null,
@@ -91,55 +148,89 @@ namespace GenericReositoryDll.Repository.GenericRepository
       return result;
     }
 
-    public async Task<T> GetSingleAsync(long id)
+
+    public List<T> GetList<T>(Expression<Func<T, bool>> query = null,
+     Func<T, object> orderBy = null,
+     OrderType? orderType = null,
+     List<string> includes = null, int? skip = 0,
+     int? take = null, bool? distinct = null)
+      => (List<T>) GetList(query, orderBy, orderType, includes, skip, take, distinct);
+
+
+    public async Task<T> FindAsync(long id)
     => await _model.FindAsync(id);
+
+    public T Find(long id)
+    =>  _model.Find(id);
 
     public async Task<object> GetSingleAsync(Expression<Func<T, bool>> query,
       Func<T,object> selector,
       List<string> includes = null)
     {
-      object result;
 
       dynamic model = _model.AsNoTrackingWithIdentityResolution().AsQueryable();
       if (includes != null && includes.Count() > 0)
         includes.ForEach(includeProperty => model.Include(includeProperty));
       if (query != null) model = model.Where(query);
-      if (selector != null) model = model.Select(selector).FirstOrDefault();
-      
+      if (selector != null) model = model.Select(selector).FirstOrDefaultAsync().Result;     
       return model;
       
     }
 
+    public object GetSingle(Expression<Func<T, bool>> query,
+     Func<T, object> selector,
+     List<string> includes = null)
+    {
+      dynamic model = _model.AsNoTrackingWithIdentityResolution().AsQueryable();
+      if (includes != null && includes.Count() > 0)
+        includes.ForEach(includeProperty => model.Include(includeProperty));
+      if (query != null) model = model.Where(query);
+      if (selector != null) model = model.Select(selector).FirstOrDefaultAsync().Result;
+      return model;
+
+    }
+
     public async Task<T> GetSingleAsync<T>(Expression<Func<T, bool>> query,
       List<string> includes = null)
-    {
-      var result = (T)GetSingleAsync(query, includes).Result;
-      return result;
-    }
+    => (T)GetSingleAsync(query, includes).Result;
+
+    public T GetSingle<T>(Expression<Func<T, bool>> query,
+      List<string> includes = null)
+    =>(T)GetSingle(query, includes);
+
 
     public async Task UpdateAsync(T model)
     {
       _model.Update(model);
       await _context.SaveChangesAsync();
     }
-
+    public void Update(T model)
+    {
+      _model.Update(model);
+      Save();
+    }
     public async Task UpdateRangeAsync(IEnumerable<T> models)
     {
       _model.UpdateRange(models);
       await _context.SaveChangesAsync();
     }
-
-    public async Task SaveAsync()
+    public  void UpdateRange(IEnumerable<T> models)
     {
-      await _context.SaveChangesAsync();
+      _model.UpdateRange(models);
+      Save();
     }
+    public async Task SaveAsync()
+    => await _context.SaveChangesAsync();   
+    public  void Save()
+    => _context.SaveChanges();
+    
 
-   
 
 
 
-   
 
-  
+
+
+
   }
 }
